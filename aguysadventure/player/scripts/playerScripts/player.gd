3863c7ca2192318d62d89fd9a242fe0e92fe60d1
Wiktor Bober <135622933+MasterMind07777@users.noreply.github.com>
2025-06-17 17:04:45 +0200

    Basic player movement (incomplete)

--- aguysadventure/player/scripts/playerScripts/player.gd
+++ aguysadventure/player/scripts/playerScripts/player.gd
@@ -1,34 +1,54 @@
 extends CharacterBody3D
 
-@export var playerSpeed: int
+@export var playerWalkingSpeed: float
+@export var playerSprintingSpeed: float
 
-var playerState: String
+@export var playerWeakJumpSpeed: float
+@export var playerStrongJumpSpeed: float
+
+var playerState: String = "Idle"
 var playerStateArray: Array = ["Idle", "Attacking", "Walking", "Sprinting", "Stunned"]
-var playerVelocity: Vector3 = Vector3()
 var playerDirection: Vector3 = Vector3()
 
-#----------------------------------------------------------------------------
 func _ready() -> void:
 	pass
 
-#----------------------------------------------------------------------------
 func _physics_process(_delta: float) -> void:
-	pass
+	if playerDirection != Vector3.ZERO:
+		setPlayerState("Walking")
+	else:
+		setPlayerState("Idle")
 
-#----------------------------------------------------------------------------
+	move()
+
 func setPlayerState(state):
 	if state in playerStateArray:
 		playerState = state
-	return
 
-#----------------------------------------------------------------------------
 func jump():
 	pass
 
-#----------------------------------------------------------------------------
 func attack():
 	pass
 
-#----------------------------------------------------------------------------
+func setDirection() -> void:
+	if Input.is_action_pressed("press_W"):
+		playerDirection.z -= 1
+	if Input.is_action_pressed("press_S"):
+		playerDirection.z += 1
+	if Input.is_action_pressed("press_D"):
+		playerDirection.x += 1
+	if Input.is_action_pressed("press_A"):
+		playerDirection.x -= 1
+	return
+
 func move():
-	pass
+	match playerState:
+		"Walking":
+			velocity = playerDirection * playerWalkingSpeed
+		"Sprinting":
+			velocity = playerDirection * playerSprintingSpeed
+		"Idle":
+			velocity = Vector3.ZERO
+	
+	move_and_slide()
