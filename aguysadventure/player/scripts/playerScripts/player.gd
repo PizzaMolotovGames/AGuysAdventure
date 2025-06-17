29629be07dde7e5b88e295b246af67d909295f45
Wiktor Bober <135622933+MasterMind07777@users.noreply.github.com>
2025-06-17 16:01:45 +0200

    Player main script setup

--- aguysadventure/player/scripts/playerScripts/player.gd
+++ aguysadventure/player/scripts/playerScripts/player.gd
@@ -1,10 +1,34 @@
 extends CharacterBody3D
 
-func _init() -> void:
-	pass
+@export var playerSpeed: int
 
+var playerState: String
+var playerStateArray: Array = ["Idle", "Attacking", "Walking", "Sprinting", "Stunned"]
+var playerVelocity: Vector3 = Vector3()
+var playerDirection: Vector3 = Vector3()
+
+#----------------------------------------------------------------------------
 func _ready() -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
+#----------------------------------------------------------------------------
+func _physics_process(_delta: float) -> void:
+	pass
+
+#----------------------------------------------------------------------------
+func setPlayerState(state):
+	if state in playerStateArray:
+		playerState = state
+	return
+
+#----------------------------------------------------------------------------
+func jump():
+	pass
+
+#----------------------------------------------------------------------------
+func attack():
+	pass
+
+#----------------------------------------------------------------------------
+func move():
 	pass
