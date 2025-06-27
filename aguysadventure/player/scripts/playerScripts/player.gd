7997e8fd3a188d8dc7e7166f28b4cf5c926b3cc7
Wiktor Bober <135622933+MasterMind07777@users.noreply.github.com>
2025-06-27 19:29:45 +0200

    Player update
    -The player now rotates towards his walking position;
    -Foundation for jumping functionality;
    -Comments

--- aguysadventure/player/scripts/playerScripts/player.gd
+++ aguysadventure/player/scripts/playerScripts/player.gd
@@ -1,17 +1,21 @@
 extends CharacterBody3D
 
+#Player pivot variable activated on the first frame
+@onready var playerPivot: Node3D = $"pivot"
+
+#Player speeds variables
 @export var playerWalkingSpeed: float
 @export var playerSprintingSpeed: float
-
 @export var playerWeakJumpSpeed: float
 @export var playerStrongJumpSpeed: float
+@export var playerFallSpeed: float
 
+#Player state variable and Array
 var playerState: String = "Idle"
-var playerStateArray: Array = ["Idle", "Attacking", "Walking", "Sprinting", "Stunned"]
-var playerDirection: Vector3 = Vector3()
+var playerStateArray: Array = ["Idle", "Attacking", "Walking", "Sprinting", "Stunned", "WeakJump", "StrongJump"]
 
-func _ready() -> void:
-	setPlayerState("Idle")
+#Player direction based on input
+var playerDirection: Vector3 = Vector3()
 
 func _physics_process(_delta: float) -> void:
 	setDirection()
@@ -21,7 +25,11 @@ func _physics_process(_delta: float) -> void:
 			setPlayerState("Sprinting")
 	else:
 		setPlayerState("Idle")
-
+	
+	if Input.is_action_pressed("press_SPACE"):
+		pass
+	
+	rotatePlayerPivot()
 	move()
 
 func setPlayerState(state):
@@ -35,17 +43,10 @@ func attack():
 	pass
 
 func checkDirectionalInput():
-	if Input.is_action_pressed("press_W"):
-		return true
-	if Input.is_action_pressed("press_S"):
-		return true
-	if Input.is_action_pressed("press_D"):
-		return true
-	if Input.is_action_pressed("press_A"):
-		return true
-	return false
+	return Input.is_action_pressed("press_W") or Input.is_action_pressed("press_S") or Input.is_action_pressed("press_D") or Input.is_action_pressed("press_A")
 
 func setDirection() -> void:
+	playerDirection = Vector3.ZERO
 	if Input.is_action_pressed("press_W"):
 		playerDirection.z = -1
 	if Input.is_action_pressed("press_S"):
@@ -55,7 +56,6 @@ func setDirection() -> void:
 	if Input.is_action_pressed("press_A"):
 		playerDirection.x = -1
 	playerDirection = playerDirection.normalized()
-	return
 
 func move():
 	match playerState:
@@ -65,5 +65,11 @@ func move():
 			velocity = playerDirection * playerSprintingSpeed
 		"Idle":
 			velocity = Vector3.ZERO
+		"WeakJump":
+			pass
 	
 	move_and_slide()
+
+func rotatePlayerPivot():
+	if playerDirection != Vector3.ZERO:
+		playerPivot.look_at(global_transform.origin + Vector3(playerDirection.x, 0, playerDirection.z), Vector3.UP)
