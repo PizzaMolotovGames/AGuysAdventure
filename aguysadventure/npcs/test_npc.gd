a09b6b7a27e76758560c89b5b97f3e500fdb345c
Wiktor Bober <135622933+MasterMind07777@users.noreply.github.com>
2025-07-13 11:36:00 +0200

    Interaction Component Update, Test NPC Update
    -Added signal functionality to the interaction component;
    -Signal test in Test NPC scene.

---
+++ aguysadventure/npcs/test_npc.gd
@@ -0,0 +1,5 @@
+extends StaticBody3D
+
+
+func _on_interaction_component_interacted() -> void:
+	print("IT WORKS!!!")
--- aguysadventure/world/worldObjects/technicalObjects/interaction_component.gd
+++ aguysadventure/world/worldObjects/technicalObjects/interaction_component.gd
@@ -1,11 +1,22 @@
 extends Node
 
 @onready var interactionCollider: Area3D = $interactionCollider
-@onready var interactionPaticle: CPUParticles3D = $InteractionParticle
+@onready var interactionParticle: CPUParticles3D = $InteractionParticle
+
+@export var interactionAreaRadius: float = 1.5
+
+signal interacted
+
+func _ready() -> void:
+	var collisionShape = $interactionCollider/CollisionShape3D
+	collisionShape.shape.radius = interactionAreaRadius
 
 func _physics_process(_delta: float) -> void:
 	for x in interactionCollider.get_overlapping_bodies():
 		if x.is_in_group("CanInteract"):
-			interactionPaticle.emitting = true
+			interactionParticle.emitting = true
 		else:
-			interactionPaticle.emitting = false
+			interactionParticle.emitting = false
+	
+	if interactionParticle.emitting and Input.is_action_just_pressed("press_E"):
+		emit_signal("interacted")
